86c420b584d990748e112010fb53484289451783
dirass254 <ndirangucornelius@gmail.com>
2025-07-28 13:29:10 +0300

    Get  optimized images.

--- src/services/Image-url.ts
+++ src/services/Image-url.ts
@@ -1,6 +1,6 @@
 const CropedImageUrl = (url: string) => {
   const target = "media/";
-  const index = url.indexOf("media/") + "media/".length;
+  const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
 
