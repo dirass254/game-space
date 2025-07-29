5e62adb3d0bdadee1d448bbcfa2d19fc5d154dcf
dirass254 <ndirangucornelius@gmail.com>
2025-07-29 07:20:25 +0300

    Display genres

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -13,7 +13,7 @@ function GameGrid() {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        gap={10}
+        gap={3}
         padding="10px"
       >
         {isLoading &&
--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,13 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../Hooks/useGenres";
+import CropedImageUrl from "../services/Image-url";
 
 function GenresList() {
-  const { data, error, isLoading } = useGenres();
+  const { data } = useGenres();
+
   return (
-    <ul>
+    <List.Root>
       {data.map((g) => (
-        <li key={g.id}>{g.name}</li>
+        <List.Item paddingY={"5px"} key={g.id}>
+          <HStack>
+            <Image
+              src={CropedImageUrl(g.image_background)}
+              boxSize="32px"
+              borderRadius={8}
+            />
+            <Text fontSize="lg">{g.name}</Text>
+          </HStack>
+        </List.Item>
       ))}
-    </ul>
+    </List.Root>
   );
 }
 
