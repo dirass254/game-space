9c2f7c33f874439b4bacc837c9065cf34e9aaf74
dirass254 <ndirangucornelius@gmail.com>
2025-07-30 07:16:05 +0300

    Show aspiner while showing genres.

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,25 +1,28 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../Hooks/useGenres";
 import CropedImageUrl from "../services/Image-url";
 
 function GenresList() {
-  const { data } = useGenres();
-
+  const { data, isLoading, error } = useGenres();
+  if (isLoading) return <Spinner />;
+  if (error) return <Text>Error loading genres</Text>;
   return (
-    <List.Root listStyleType={"none"}>
-      {data.map((g) => (
-        <List.Item paddingY={"5px"} key={g.id}>
-          <HStack>
-            <Image
-              src={CropedImageUrl(g.image_background)}
-              boxSize="32px"
-              borderRadius={8}
-            />
-            <Text fontSize="lg">{g.name}</Text>
-          </HStack>
-        </List.Item>
-      ))}
-    </List.Root>
+    <>
+      <List.Root listStyleType={"none"}>
+        {data.map((g) => (
+          <List.Item paddingY={"5px"} key={g.id}>
+            <HStack>
+              <Image
+                src={CropedImageUrl(g.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+              />
+              <Text fontSize="lg">{g.name}</Text>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
   );
 }
 
